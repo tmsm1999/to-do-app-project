482ebcf80e978dac5c3bdb78c5cbc9ffed844245
Tomás Mamede <tomas.sant.mamede@icloud.com>
2025-04-20 12:36:30 +0100

    start project

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Tomas Mamede on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
